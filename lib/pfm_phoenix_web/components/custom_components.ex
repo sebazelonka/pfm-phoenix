efb8a2301db8e0145d4a81e694ff79ff28c1051e
Sebastian Zelonka <dev@zlnk.com>
2024-09-13 13:29:01 -0300

    grid on home

--- lib/pfm_phoenix_web/components/custom_components.ex
+++ lib/pfm_phoenix_web/components/custom_components.ex
@@ -2,15 +2,10 @@
 defmodule PfmPhoenixWeb.CustomComponents do
   use Phoenix.Component
 
-  attr :type, :string, default: nil
-  attr :class, :string, default: nil
-  attr :rest, :global, include: ~w(disabled form name value)
-  slot :inner_block, required: true
-
   def customChart(assigns) do
     ~H"""
-    <div class="flex flex-col items-center justify-center">
-      <canvas id="myChart" width="400" height="400" phx-hook="ChartJS"></canvas>
+    <div class="flex flex-col items-center justify-center w-full">
+      <canvas id="myChart" phx-hook="ChartJS" class="w-96 h-96"></canvas>
     </div>
     """
   end
