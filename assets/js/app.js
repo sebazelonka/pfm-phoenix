53e345dc694839ab1ae79e6ea8d980941fb300c2
Sebastian Zelonka <dev@zlnk.com>
2024-09-13 10:16:58 -0300

    code cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,23 +21,7 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
-import Chart from "chart.js";
-
-let hooks = {};
-hooks.ChartJS = {
-  mounted() {
-    const ctx = this.el;
-    const data = {
-      type: "bar",
-      data: {
-        // random data to validate chart generation
-        labels: ["A", "B", "C", "D", "E"],
-        datasets: [{ data: [14, 3, 8, 2, 1] }],
-      },
-    };
-    const chart = new Chart(ctx, data);
-  },
-};
+import { hooks } from "./hooks";
 
 // and add the hook definition
 
