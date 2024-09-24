5b812cfcd4232a89ca61dbc33bf356f6acb9e1d1
Sebastian Zelonka <dev@zlnk.com>
2024-09-24 14:52:11 -0300

    update gettext usage https://github.com/elixir-gettext/gettext/blob/main/CHANGELOG.md

--- lib/pfm_phoenix_web/gettext.ex
+++ lib/pfm_phoenix_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule PfmPhoenixWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :pfm_phoenix
+  use Gettext.Backend, otp_app: :pfm_phoenix
 end
