b18b32dc9778f9d67cc85e822e82ad3b0eb2acc2
Sebastian Zelonka <dev@zlnk.com>
2024-10-07 10:27:12 -0300

    expenses working with budgets

--- lib/pfm_phoenix/transactions/expense.ex
+++ lib/pfm_phoenix/transactions/expense.ex
@@ -8,6 +8,7 @@ defmodule PfmPhoenix.Transactions.Expense do
     field :category, :string
     field :amount, :decimal
     belongs_to :user, PfmPhoenix.Accounts.User
+    belongs_to :budget, PfmPhoenix.Finance.Budget
 
     timestamps(type: :utc_datetime)
   end
@@ -15,8 +16,9 @@ defmodule PfmPhoenix.Transactions.Expense do
   @doc false
   def changeset(expense, attrs) do
     expense
-    |> cast(attrs, [:amount, :description, :date, :category, :user_id])
-    |> validate_required([:amount, :description, :date, :category, :user_id])
+    |> cast(attrs, [:amount, :description, :date, :category, :user_id, :budget_id])
+    |> validate_required([:amount, :description, :date, :category, :user_id, :budget_id])
     |> foreign_key_constraint(:user_id)
+    |> foreign_key_constraint(:budget_id)
   end
 end
